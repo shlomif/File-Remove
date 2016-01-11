d5a276941a1c93ce5e5251135f0a282dea28d64f
Shlomi Fish <shlomif@shlomifish.org>
2016-01-11 16:10:34 +0200

    Bump version to 1.55

--- lib/File/Remove.pm
+++ lib/File/Remove.pm
@@ -6,7 +6,7 @@ use strict;
 use vars qw{ $VERSION @ISA @EXPORT_OK };
 use vars qw{ $DEBUG $unlink $rmdir    };
 BEGIN {
-	$VERSION   = '1.54';
+	$VERSION   = '1.55';
 	# $VERSION   = eval $VERSION;
 	@ISA       = qw{ Exporter };
 	@EXPORT_OK = qw{ remove rm clear trash };
